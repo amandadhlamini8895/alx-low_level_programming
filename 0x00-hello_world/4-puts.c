7dae5838beb6b7f2a6cdb39d21b5cfe2625473a6
Amandadhlamini8895 <amandamqotsi@gmail.com>
2023-07-13 12:09:25 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 /**
  * main - A program thatt prints a line with puts function
  * Return: 0 (Success)
- */
 int main(void)	
 {	
-puts("/"Programming is like building a multilingual puzzle");			
-return (0);	
+puts("/"Programming is like building a multilingual";		
+return (0);		
 }	
