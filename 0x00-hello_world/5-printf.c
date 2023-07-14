74bb592b53a63c1e7c516ff634713d7983da2a6e
Amandadhlamini8895 <amandamqotsi@gmail.com>
2023-07-14 04:22:15 -0700

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,5 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s) \ n", sizeof (char));
-	printf("Size of a int: %d byte(s) \ n", sizeof(int));
-	printf("Size of a long int %d byte(s) \ n", sizeof(long int));
-	printf("Size of a long long int %d byte(s) \ n, sizof(long long int));
-	return (0);
+	printf("with proper grammar, but the outcomeis a pieceof art \ n");
 }	
